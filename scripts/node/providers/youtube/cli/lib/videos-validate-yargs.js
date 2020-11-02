f77175c5aa26c96df4d0ee63876948edcf290902
apolopena <3060702+apolopena@users.noreply.github.com>
2020-11-02 10:00:12 -0800

    enquirer

---
+++ scripts/node/providers/youtube/cli/lib/videos-validate-yargs.js
@@ -0,0 +1,36 @@
+const COMMAND_NAMES = ['seed']
+let program, _logger
+
+module.exports = validateYargs
+
+function fatal(msg, l = _logger, p = program) {
+  p.showHelp()
+  l.error(`Error: ${p.$0} ${msg}`)
+  process.exit(1)
+}
+
+function validateYargs(yargsProgram, processArgv, logger) {
+  program = yargsProgram
+  _logger = logger
+
+  const rawArgv = processArgv
+  const commands = program.argv._
+  const argv = program.argv
+
+  commands.length == 0 && !argv.i && fatal(': Only the --interactive option can be used without a command.')
+  commands.length > 0 && argv.i && fatal(': Commands cannot be given when the --interative option is set.')
+  commands.length > 1 && fatal(`: Only one command can be given at a time. Found the commands ${commands.join(' ')}`) 
+  if (commands.length == 0 || !commands.includes('seed')) {
+    argv.f && fatal(': The --file option can only be used with the seed command.')
+  }
+  // -f, --file option is required with the seed command and cannot be empty
+  if (commands.includes('seed')) {
+    filePathIndex = rawArgv.indexOf('-f')
+    filePathIndex === -1 && (filePathIndex = rawArgv.indexOf('--file'))
+    filePathIndex === -1 && fatal(': seed command requires the --file option.')
+    rawArgv[filePathIndex + 1] || fatal (': The --file option cannot be empty.')
+    //fs.existsSync(argv.file) || fatal(`: the file ${path.resolve(argv.file)} does not exist.`)
+  }
+  !argv.i && !COMMAND_NAMES.includes(commands[0]) && fatal(`invalid command ${commands[0]}`)
+}
+
--- scripts/node/providers/youtube/cli/videos-cli.js
+++ scripts/node/providers/youtube/cli/videos-cli.js
@@ -13,25 +13,22 @@
  * @license MIT
  */
 
+const fs = require('fs')
 const path = require('path')
 
+const yargs = require('yargs')
 const chalk = require('chalk')
 
 const sharedLibRoot = path.resolve(__dirname, '../../../')
+const stubRoot = path.resolve(__dirname, '../test/stub/network-response/')
 const loggerUri = path.resolve(sharedLibRoot, 'local-console-logger.js')
 
 const logger = require(loggerUri).console
-const cl = require('./lib/color-console-log').trueColor.pastelOne
-const colorizeYargs = require('./lib/yargs-colorizer.js')
+//const cl = require('./lib/color-console-log').trueColor.pastelOne
+const colorizeYargs = require('./lib/yargs-colorizer.js');
+const validateYargs = require('./lib/videos-validate-yargs.js')
 
-const C = {
-  rose: '#FFAFFF',
-  cornflower: '#AFD7FF',
-  mint: '#AFFFD7',
-  peach: '#FFD7AF',
-  shalimar: '#FFFFAF',
-  pink: '#FFAFD7'
-}
+const C = require('./lib/custom-hex-colors').palettes.pastelOne
 
 const c = {
   rose: chalk.hex(C.rose),
@@ -39,7 +36,83 @@ const c = {
   mint: chalk.hex(C.mint),
   peach: chalk.hex(C.peach),
   shalimar: chalk.hex(C.shalimar),
-  pink: chalk.hex(C.pink)
+  pink: chalk.hex(C.pink),
+  salmon: chalk.hex(C.salmon)
 }
+
+const NAME = 'videos-cli'
+const VERSION = `${NAME} 0.1.2`
+const COMMAND_NAMES = ['seed']
+
+
+const options = {
+  interactive: {
+    alias: 'interactive',
+    default: false,
+    describe: c.mint('Runs the tool interactively. Cannot be used in conjunction with any command.'),
+    type: 'boolean'
+  },
+  file: {
+    alias: 'file',
+    describe: c.mint('Specifies a file to seed rather than gathering the search lists from the terms data in the Firestore.'),
+    type: 'string'
+  },
+  dryRun: {
+    alias: 'dryRun',
+    describe: c.mint('Specifies if the seeding operation should skip https requests. Used for testing.'),
+    default: false,
+    type: 'boolean'
+  },
+  dryRunVideoCount: {
+    alias: 'dryRunVideoCount',
+    describe: c.mint('Fakes the number of videos received when the dryRun option is set.'),
+    default: 5,
+    type: 'number'
+  },
+  skipVideoRequests: {
+    alias: ['svr', 'skipVideoRequests'],
+    describe: c.mint('Skips the only the https video requests during a seeding operation. Ignored when the dryRun option is set.'),
+    default: false,
+    type: 'boolean'
+  },
+  useNetworkStub: {
+    alias: ['uns','useNetworkStub'],
+    describe: c.mint(`Uses fake data for https search and video requests. ` +
+    `This option is ignored if the dryRun option is set. Assumes that the directory ${c.salmon(
+    stubRoot)} exists and that ${c.salmon('search-list.json')} exists there for any search list requests made ` +
+    `and that ${c.salmon('video-list.json')} exists there for any video requests made.`),
+    default: false,
+    type: 'boolean'
+  }
+}
+
+const program = require('yargs/yargs')(process.argv.slice(2))
+  .scriptName(c.cornflower(NAME))
+  .version(c.cornflower(VERSION))
+  .epilog(c.shalimar('\n© 2020 Devz3n.com'))
+  .option('i', options.interactive)
+  .option('f', options.file)
+  .option('d', options.dryRun)
+  .option('dryVideos', options.dryRunVideoCount)
+  .option('s', options.skipVideoRequests)
+  .option('u', options.useNetworkStub)
+  .command({
+    command: 'seed',
+    desc: 'Seeds a Firestore',
+    builder: (yargs) => {},
+    handler: (argv) => {
+      console.log(`SEEDING...`)
+    }
+  })
+  .wrap(80)
+  .help();
+
+const main = async(p = program) => {
+  colorizeYargs.pastelColor(program)
+  validateYargs(p, process.argv, logger)
+}
+// BEGIN: Main Program
+main(program)
+// END: Main Program
 
 
