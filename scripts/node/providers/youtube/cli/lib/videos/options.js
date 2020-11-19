34636f16464d8bab78be47c3c5977b18308d6a1a
apolopena <3060702+apolopena@users.noreply.github.com>
2020-11-19 09:12:55 -0800

    improved video options

--- scripts/node/providers/youtube/cli/lib/videos/options.js
+++ scripts/node/providers/youtube/cli/lib/videos/options.js
@@ -2,52 +2,35 @@
  * @fileoverview Yargs option objects for videos-cli.
  * @author <a href="maito:apolo4pena@gmail.com">Apolo Pena</a>
  * @description Modularizes options for the videos-cli command line tool.
+ * Utilizes JSON Schema for options validation and data.
  * @requires path
  * @requires yargs-colorizer
  * @license MIT
  */
 
 const path = require('path')
-const c = require('../yargs-colorizer').pastelColorStrings
-const stubRoot = path.resolve(__dirname, '../../test/stub/network-response/')
+
+const {
+  pastelColor,
+  pastelColorStrings: c 
+} = require('../yargs-colorizer');
+
+const schema = require('./options-schema.json')
+
+const options = Object.keys(schema.properties).reduce((acc, key) => {
+  const val = schema.properties[key]
+  acc[key] = val
+  acc[key].description = c.mint(
+    `${acc[key].description}${(val.comment) 
+      ? (' ' + c.coral(acc[key].comment))
+      : '' }`
+  )
+  acc[key].alias = key
+  return acc
+}, {})
 
 module.exports = {
-  interactive: {
-    alias: 'interactive',
-    default: false,
-    describe: c.mint('Runs the tool interactively. Cannot be used in conjunction with any command.'),
-    type: 'boolean'
-  },
-  file: {
-    alias: 'file',
-    describe: c.mint('Specifies a file to seed rather than gathering the search lists from the terms data in the Firestore.'),
-    type: 'string'
-  },
-  dryRun: {
-    alias: 'dryRun',
-    describe: c.mint('Specifies if the seeding operation should skip https requests. Used for testing.'),
-    default: false,
-    type: 'boolean'
-  },
-  dryRunVideoCount: {
-    alias: 'dryRunVideoCount',
-    describe: c.mint('Fakes the number of videos received when the dryRun option is set.'),
-    default: 5,
-    type: 'number'
-  },
-  skipVideoRequests: {
-    alias: ['svr', 'skipVideoRequests'],
-    describe: c.mint('Skips the only the https video requests during a seeding operation. Ignored when the dryRun option is set.'),
-    default: false,
-    type: 'boolean'
-  },
-  useNetworkStub: {
-    alias: ['uns','useNetworkStub'],
-    describe: c.mint(`Uses fake data for https search and video requests. ` +
-    `This option is ignored if the dryRun option is set. Assumes that the directory ${c.salmon(
-    stubRoot)} exists and that ${c.salmon('search-list.json')} exists there for any search list requests made ` +
-    `and that ${c.salmon('video-list.json')} exists there for any video requests made.`),
-    default: false,
-    type: 'boolean'
-  }
+  data: options,
+  stringColorizer: c,
+  yargsColorizer: pastelColor
 }
