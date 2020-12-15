43f2294abe6cf81a42173ff96e061db50e919585
apolopena <3060702+apolopena@users.noreply.github.com>
2020-12-15 10:55:18 -0800

    new setups (#47)
    * Bump fuse.js from 6.4.1 to 6.4.3
    
    Bumps [fuse.js](https://github.com/krisk/Fuse) from 6.4.1 to 6.4.3.
    - [Release notes](https://github.com/krisk/Fuse/releases)
    - [Changelog](https://github.com/krisk/Fuse/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/krisk/Fuse/compare/v6.4.1...v6.4.3)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * Bump firebase from 7.20.0 to 8.2.0
    
    Bumps [firebase](https://github.com/firebase/firebase-js-sdk) from 7.20.0 to 8.2.0.
    - [Release notes](https://github.com/firebase/firebase-js-sdk/releases)
    - [Changelog](https://github.com/firebase/firebase-js-sdk/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/firebase/firebase-js-sdk/commits)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    * hid firebase config
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,20 +1,10 @@
 import Firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-// import { seedDatabase } from '../seed'
+import { config } from './firebase.config.prod.js' // .gitignored for security, as a dev how to make one
+//import { seedDatabase } from '../seed'
 
 // need to somehow seed the database
-
-// need config
-const config = {
-  apiKey: 'AIzaSyA6DK4TpETqUxK_WLOtLCT_zhldeuv8R1Q',
-  authDomain: 'devflix-3e3a5.firebaseapp.com',
-  databaseURL: 'https://devflix-3e3a5.firebaseio.com',
-  projectId: 'devflix-3e3a5',
-  storageBucket: 'devflix-3e3a5.appspot.com',
-  messagingSenderId: '1016430120320',
-  appId: '1:1016430120320:web:c41b2ea4550970d7038565'
-}
 
 const firebase = Firebase.initializeApp(config)
 // !!!!!!!!!!!!!!!! DO NOT UN-COMMENT THIS SEED !!!!!!!!!!!!!
